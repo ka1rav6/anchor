53726391540f64437d9444325b987a6486f35963
Kairav <kairavdutta@gmail.com>
2026-06-18 20:33:15 +0530

    created basic structure and functions for the arena (functions yet to be implemented)

---
+++ src/arena.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <stddef.h>
+
+// my own arena style memory allocator used ONLY in rule allocation and AST
+// As AST is sequential and is freed and created once.
+
+
+typedef struct{
+    size_t size;
+    size_t used;
+    char* start;
+}Arena;
+
+
+char* ask_memory(size_t);
+Arena* create_arena(size_t);
+
+void* arena_alloc(Arena*, size_t, void* data);
+void arena_free(Arena*);
