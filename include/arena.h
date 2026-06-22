f55a72c14e5f6f32ee3e09ec3a7fa50e0deffd55
Kairav <kairavdutta@gmail.com>
2026-06-22 16:48:45 +0530

    encapsulated arena by making an internal header and added thread-safety by adding a lock to the struct

--- include/arena.h
+++ include/arena.h
@@ -1,20 +1,13 @@
 #pragma once
 #include "common.h"
-#include <string.h>
-#include <sys/mman.h>
 #include <stddef.h>
 
-#define ARENA_ALIGNMENT 8
+/* Opaque handle: callers cannot see or touch the fields of an Arena.
+ * The only way to operate on an Arena is through the functions below. */
+typedef struct Arena Arena;
 
-typedef struct {
-    size_t size;
-    size_t used;
-    char*  start;
-} Arena;
-
-char* ask_memory(size_t size);
 Arena* createArena(size_t size);
-void* arena_alloc(Arena* ar, size_t size);
-char* arena_strdup(Arena* ar, const char* s);
-void arena_reset(Arena* ar);
-void destroyArena(Arena* ar);
+void*  arena_alloc(Arena* ar, size_t size);
+char*  arena_strdup(Arena* ar, const char* s);
+void   arena_reset(Arena* ar);
+void   destroyArena(Arena* ar);
